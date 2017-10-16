7a08fcb84464c36b253196c5b36fea7ce1f31d73
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-16 11:02:05 -0400

    Add Ranking Col to Tracks Table

--- chinook/app/models/track.rb
+++ chinook/app/models/track.rb
@@ -1,2 +1,67 @@
 class Track < ApplicationRecord
-end
+
+  SHORT = 180000
+  LONG = 360000
+
+  scope  :short, ->  { shorter_than (SHORT)}   # a proc, only for class methods, to narrow down your query
+
+  def self.shorter_than(milliseconds)
+    where('milliseconds < ?', milliseconds)
+  end
+
+  scope :long, -> { longer_than_or_equal_to (LONG)}
+
+  scope :medium, -> {longer_than_or_equal_to (SHORT).shorter_than(LONG)}
+
+  scope :shorter_than, -> (milliseconds) { where ('milliseconds < ?', milliseconds)} if milliseconds.present? && milliseconds > 0 }
+
+  scope :longer_than_or_equal_to-> (milliseconds) { where ('milliseconds ? > ?', milliseconds)}
+
+  scope :starts_with, -> (char) { where ('name ILIKE ?', "#{ char }%") }
+
+
+
+#
+#   # def self.short
+#   #   #where('milliseconds < ?', SHORT)
+#   #   shorter_than(SHORT)
+#   # end
+#
+#   def self.long
+#     #where('milliseconds >= ?', LONG)
+#     longer_than_or_equal_to(LONG)
+#   end
+#
+#   def self.medium
+#     #where('milliseconds >= ?', SHORT).where('milliseconds < ? ', SHORT)
+#     longer_than_or_equal_to(SHORT).shorter_than(LONG)
+#   end
+#
+#
+#
+#   def self.longer_than_or_equal_to(milliseconds)
+#     where('milliseconds >= ?', milliseconds)
+#   end
+#
+#   def self.starts_with(char)
+#
+#     where('name ILIKE ?', "#{ char }%")  #% is wildcard
+#
+#   end
+#
+#
+# end
+#
+#
+#
+# # def self.short
+# #   where('milliseconds < ?', SHORT)
+# # end
+# #
+# # def self.long
+# #   where('milliseconds >= ?', LONG)
+# # end
+# #
+# # def self.medium
+# #   where('milliseconds >= ?', SHORT).where('milliseconds < ? ', SHORT)
+# # end
